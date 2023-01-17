1d52b9d53c4a791359656004e9332e6ab3350c89
Rodger Li <rodli@redhat.com>
2023-01-17 22:25:36 +1100

    update: added data points

--- bar_chart.py
+++ bar_chart.py
@@ -7,7 +7,7 @@ axis_length = 400
 origin_x = window_width/2.5
 origin_y = window_width/2.5
 
-# data_points = turtle.numinput('Total count', 'How many data points?', minval=0, maxval=20)
+data_points_input = turtle.numinput('Total count', 'How many data points?', minval=0, maxval=20)
 
 # Set up the turtle
 t = turtle.Turtle()
@@ -17,7 +17,7 @@ t.speed(10)
 t.width(3)
 
 # Disable turtle animations
-turtle.tracer(5)
+# turtle.tracer(5)
 
 # Set the window size
 turtle.setup(window_width, window_height)
@@ -31,19 +31,43 @@ def create_triangle(deviation_angle=30, triangle_angle=120, distance=window_heig
     t.end_fill()
     t.left(deviation_angle)
 
+def create_axis(data_points=6):
+    for i in range(int(data_points + 1 )):
+        if i == int(data_points + 1):
+            break
+        else:
+            t.forward(axis_length/(data_points + 1 ))
+            t.right(90)
+            t.forward(axis_length/40)
+            t.back(axis_length/40)
+            t.left(90)
+
 # Set turtle starting position
 t.back(origin_x)
 t.right(90)
 t.back(origin_y)
 t.color("black")
-# Create y-axis triangle. Requires deviation_angle=30 (default)
-create_triangle()
-#Create L
-t.forward(350)
+
+create_triangle() # Creates y-axis triangle. Requires deviation_angle=30 (default)
+create_axis()
 t.left(90)
-t.forward(350)
+create_axis(data_points_input)
+###
+# t.forward(axis_length)
+###
 # Create x-axis triangle. Requires deviation_angle=210.
 create_triangle(210)
+t.back(200)
+# for d in range(int(data_points_input + 1)):
+#     distance_back = axis_length / (data_points_input + 1)
+#     if d==(data_points_input + 1):
+#         break
+#     t.back(distance_back)
+#     t.right(90)
+#     t.forward(axis_length/40)
+#     t.back(axis_length/40)
+#     t.left(90)
+    
 
 # Show the drawing
 turtle.done()
